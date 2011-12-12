597888dd30751cc18e8b7588fa10b94399531869
Swaroop Ramaswamy <swaroop551992@gmail.com>
2011-12-12 21:07:54 +0530

    Wrote first draft of scanpath function to scan directory

--- scanner/models.py
+++ scanner/models.py
@@ -1,13 +1,13 @@
 from django.db import models
 
-class Movie(models.Model):
+class Film(models.Model):
     title     = models.CharField(max_length = 1000)
     rating    = models.CharField(max_length = 3)
     director  = models.CharField(max_length = 2000)
-    runtime   = models.CharField(max_length = 20))
+    runtime   = models.CharField(max_length = 20)
     summary   = models.CharField(max_length = 10000)
     cast      = models.CharField(max_length = 10000)
-    coverurl  = models.URLfield()
+    coverurl  = models.URLField()
     genre     = models.CharField(max_length = 100)
 
     def __unicode__(self):
